cd890af74397af8a6160a3a9d9e247051358fa15
Ksenia Pavlova <pavksan@gmail.com>
2022-02-10 17:14:47 +0700

    Найти максимальное из пяти чисел

---
+++ task_2.py
@@ -0,0 +1,14 @@
+# 2. Найти максимальное из пяти чисел
+#import random 
+#number_list = []
+#for i in range(5):
+    #number_list.append(random.randint(1,10))
+#print(number_list)
+#print(f'max number it is: {max(number_list)}')
+
+a=2
+b=3
+c=6
+d=78
+e=55
+print(max(a,b,c,d,e))
---
+++ task_6.py
@@ -0,0 +1,8 @@
+# Дано число обозначающее день недели. 
+# Вывести его название и указать является ли он выходным
+list=['Monday', 'Tuesday', 'Wensday', 'Thursday', 'Friday','Saturday', 'Sunday']
+day = int(intput('enter a number from 1 to 7'))
+if list[i]== 5 or 6:
+print('Weekend')
+else:
+    print('Weekday')
